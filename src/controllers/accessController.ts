e965b8b1ce8a0f3a0e5199d752ce1ec41f32a08c
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-08-02 22:36:36 -0300

    feat: signs up user

--- src/controllers/accessController.ts
+++ src/controllers/accessController.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 
-import { CreateUser } from "../services/accessService.js";
+import accessService, { CreateUser } from "../services/accessService.js";
 
 export async function signUp(req: Request, res: Response) {
     const user: CreateUser = res.locals.data;
-    console.log(user)
+    await accessService.signUpUser(user);
     res.sendStatus(201);
 }
--- src/services/accessService.ts
+++ src/services/accessService.ts
@@ -1,7 +1,27 @@
 import { User } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+import accessRepository from "../repositories/accessRepository.js";
 
 export type CreateUser = Omit<User & 
     { repeatedPassword?: string },
     "id" | 
     "createdAt"
 >
+
+async function signUpUser(user: CreateUser) {
+    delete user.repeatedPassword;
+    const encryptedPassword = encryptPassword(user.password);
+    await accessRepository.saveUserData({ ...user, password: encryptedPassword });
+}
+
+function encryptPassword(password: string) {
+    const SALT = 10;
+    return bcrypt.hashSync(password, SALT);
+}
+
+const accessService = {
+    signUpUser,
+};
+
+export default accessService;
